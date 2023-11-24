5f12a0c4ea30ee5315f33c8377f2191547bbf3ec
nkunz <nkunz@seemoo.tu-darmstadt.de>
2023-11-24 21:15:21 +0100

    Add sample graph with edge labels

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { Edges } from 'v-network-graph';
+import { VNetworkGraph, VEdgeLabel } from 'v-network-graph';
+
+  const nodes = {
+    node1: { name: "Node 1" },
+    node2: { name: "Node 2" },
+    node3: { name: "Node 3" },
+    node4: { name: "Node 4" },
+  }
+
+  const edges : Edges = {
+    edge1: { source: "node1", target: "node2", label: "Edge 1"},
+    edge2: { source: "node2", target: "node3" },
+    edge3: { source: "node3", target: "node4" },
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-network-graph
+    class="graph"
+    :nodes="nodes"
+    :edges="edges"
+  >
+    <template #edge-label="{ edge, ...slotProps }">
+      <v-edge-label :text="edge.label" align="center" vertical-align="above" v-bind="slotProps" />
+    </template>
+  </v-network-graph>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.graph {
+  width: 800px;
+  height: 600px;
+  border: 1px solid #000;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+import VNetworkGraph from 'v-network-graph'
+import 'v-network-graph/lib/style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VNetworkGraph)
+app.mount('#app')
