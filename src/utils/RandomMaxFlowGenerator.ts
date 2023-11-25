7aafad80224bf3e3b553752622e655dd974ab10f
nkunz <nkunz@seemoo.tu-darmstadt.de>
2023-11-25 23:33:45 +0100

    Add RandomMaxFlowGenerator

---
+++ src/utils/RandomMaxFlowGenerator.ts
@@ -0,0 +1,17 @@
+interface Size {
+    width: number;
+    height: number;
+}
+
+interface Coordinate {
+    x: number;
+    y: number;
+}
+
+/**
+ * Generates a random max-flow problem graph with maximal planarity and strong connectivity.
+ */
+export function makeRandomMaxFlowGraph(numberOfNodes: number, maxCapacity: number, squareSize?: Size, radius = 16) {
+    
+}
+
