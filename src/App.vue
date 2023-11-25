c4ba08420092e4b939b89ad96de184f40885c013
nkunz <nkunz@seemoo.tu-darmstadt.de>
2023-11-25 23:35:32 +0100

    Add controls to sample graph

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <script setup lang="ts">
-import { Edges } from 'v-network-graph';
+import { Edges, Layouts, Nodes } from 'v-network-graph';
 import { VNetworkGraph, VEdgeLabel } from 'v-network-graph';
+import { reactive } from 'vue';
 
-  const nodes = {
-    node1: { name: "Node 1" },
-    node2: { name: "Node 2" },
-    node3: { name: "Node 3" },
-    node4: { name: "Node 4" },
-  }
+  const nodes : Nodes = reactive({
+    node1: { name: "Node 1"},
+    node2: { name: "Node 2"},
+    node3: { name: "Node 3"},
+    node4: { name: "Node 4"}
+  })
 
-  const edges : Edges = {
+  const layout : Layouts = reactive({
+    nodes: {
+      node1: { x: 0, y: 100 },
+      node2: { x: 200, y: 200 },
+      node3: { x: 300, y: 300 },
+      node4: { x: 700, y: 400 },
+    },
+  })
+
+  const edges : Edges = reactive({
     edge1: { source: "node1", target: "node2", label: "Edge 1"},
     edge2: { source: "node2", target: "node3" },
     edge3: { source: "node3", target: "node4" },
+    edge4: { source: "node2", target: "node1" },
+  })
+
+  function addNode() {
+    const newNode = `node${Object.keys(nodes).length + 1}`;
+    nodes[newNode] = { name: newNode };
+    layout.nodes[newNode] = { x: 0, y: 0 };
+    edges[`edge${Object.keys(edges).length + 1}`] = { source: "node1", target: newNode };
   }
 </script>
 
@@ -21,11 +39,15 @@ import { VNetworkGraph, VEdgeLabel } from 'v-network-graph';
     class="graph"
     :nodes="nodes"
     :edges="edges"
+    :layouts="layout"
   >
     <template #edge-label="{ edge, ...slotProps }">
       <v-edge-label :text="edge.label" align="center" vertical-align="above" v-bind="slotProps" />
     </template>
   </v-network-graph>
+  <button @click="layout.nodes.node1.x += 10">Move node 1</button>
+  <button @click="nodes.node1.name = 'Node 1 changed'">Change node 1 name</button>
+  <button @click="addNode">Add node</button>
 </template>
 
 <style scoped>
