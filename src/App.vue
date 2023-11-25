ac37e9a622a9673679c122e0ece80ff6acf9ef8d
nkunz <nkunz@seemoo.tu-darmstadt.de>
2023-11-26 00:11:11 +0100

    Render random node generation in Graph

--- src/App.vue
+++ src/App.vue
@@ -2,34 +2,36 @@
 import { Edges, Layouts, Nodes } from 'v-network-graph';
 import { VNetworkGraph, VEdgeLabel } from 'v-network-graph';
 import { reactive } from 'vue';
+import { getRandomCoordinatesWihoutIntersection } from './utils/RandomMaxFlowGenerator';
 
   const nodes : Nodes = reactive({
-    node1: { name: "Node 1"},
-    node2: { name: "Node 2"},
-    node3: { name: "Node 3"},
-    node4: { name: "Node 4"}
+
   })
 
   const layout : Layouts = reactive({
     nodes: {
-      node1: { x: 0, y: 100 },
-      node2: { x: 200, y: 200 },
-      node3: { x: 300, y: 300 },
-      node4: { x: 700, y: 400 },
+
     },
   })
 
+  const n = 100;
+  const coords = getRandomCoordinatesWihoutIntersection(n, { width: 800, height: 600 })
+  for (let i = 0; i < coords.length; i++) {
+    const newNode = `node${i + 1}`;
+    nodes[newNode] = { name: newNode };
+    layout.nodes[newNode] = { x: coords[i].x, y: coords[i].y };
+  }
+  
+
   const edges : Edges = reactive({
-    edge1: { source: "node1", target: "node2", label: "Edge 1"},
-    edge2: { source: "node2", target: "node3" },
-    edge3: { source: "node3", target: "node4" },
-    edge4: { source: "node2", target: "node1" },
+
   })
 
-  function addNode() {
+
+  function addNode(test = 0) {
     const newNode = `node${Object.keys(nodes).length + 1}`;
     nodes[newNode] = { name: newNode };
-    layout.nodes[newNode] = { x: 0, y: 0 };
+    layout.nodes[newNode] = { x: 10 + test, y: 10 };
     edges[`edge${Object.keys(edges).length + 1}`] = { source: "node1", target: newNode };
   }
 </script>
