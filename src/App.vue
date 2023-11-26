dbaf0e5c85732bf52fe1e015443304d50e69cd1b
nkunz <nkunz@seemoo.tu-darmstadt.de>
2023-11-26 23:27:45 +0100

    Enable node scaling with zoom

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-import { Edges, Layouts, Nodes } from 'v-network-graph';
+import { Edges, Layouts, Nodes, defineConfigs } from 'v-network-graph';
 import { VNetworkGraph, VEdgeLabel } from 'v-network-graph';
 import { reactive } from 'vue';
-import { getRandomCoordinatesWihoutIntersection } from './utils/RandomMaxFlowGenerator';
+import { getRandomNodes } from './utils/RandomMaxFlowGenerator';
 
   const nodes : Nodes = reactive({
-
+    
   })
 
   const layout : Layouts = reactive({
@@ -14,8 +14,15 @@ import { getRandomCoordinatesWihoutIntersection } from './utils/RandomMaxFlowGen
     },
   })
 
+  const configs = reactive(
+    defineConfigs({
+      view: {
+        scalingObjects: true,
+      },
+  })
+)
   const n = 100;
-  const coords = getRandomCoordinatesWihoutIntersection(n, { width: 800, height: 600 })
+  const coords = getRandomNodes(n, { width: 800, height: 600 })
   for (let i = 0; i < coords.length; i++) {
     const newNode = `node${i + 1}`;
     nodes[newNode] = { name: newNode };
@@ -28,11 +35,11 @@ import { getRandomCoordinatesWihoutIntersection } from './utils/RandomMaxFlowGen
   })
 
 
-  function addNode(test = 0) {
+  function addNode() {
     const newNode = `node${Object.keys(nodes).length + 1}`;
     nodes[newNode] = { name: newNode };
-    layout.nodes[newNode] = { x: 10 + test, y: 10 };
-    edges[`edge${Object.keys(edges).length + 1}`] = { source: "node1", target: newNode };
+    layout.nodes[newNode] = { x: 32, y: 40 };
+    //edges[`edge${Object.keys(edges).length + 1}`] = { source: "node1", target: newNode };
   }
 </script>
 
@@ -42,12 +49,13 @@ import { getRandomCoordinatesWihoutIntersection } from './utils/RandomMaxFlowGen
     :nodes="nodes"
     :edges="edges"
     :layouts="layout"
+    :configs="configs"
   >
     <template #edge-label="{ edge, ...slotProps }">
       <v-edge-label :text="edge.label" align="center" vertical-align="above" v-bind="slotProps" />
     </template>
   </v-network-graph>
-  <button @click="layout.nodes.node1.x += 10">Move node 1</button>
+  <button @click="layout.nodes.node1.y -= 10">Move node 1</button>
   <button @click="nodes.node1.name = 'Node 1 changed'">Change node 1 name</button>
   <button @click="addNode">Add node</button>
 </template>
