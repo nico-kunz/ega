0d348e5c24fa2ca95fe909fe13174883bfdf8771
nkunz <nkunz@seemoo.tu-darmstadt.de>
2023-11-26 23:27:15 +0100

    Adjust distance between two points, and between points and border

--- src/utils/RandomMaxFlowGenerator.ts
+++ src/utils/RandomMaxFlowGenerator.ts
@@ -11,17 +11,18 @@ interface Coordinate {
 /**
  * Generates a random max-flow problem graph with maximal planarity and strong connectivity.
  */
-export function makeRandomMaxFlowGraph(numberOfNodes: number, maxCapacity: number, squareSize?: Size, radius = 16) {
+/*export function makeRandomMaxFlowGraph(numberOfNodes: number, maxCapacity: number, squareSize?: Size, radius = 16) {
     
-}
+}*/
 
 /**
  * Get `n` random coordinates in a square of size `squareSize`.
  * @param n Number of coordinates to generate.
  * @param squareSize Size of the square, limits the max value of the coordinates.
- * @returns Array of n coordinates in the square.
+ * @param radius Radius of a node to be considered.
+ * @returns Array of n node coordinates in the square.
  */
-export function getRandomCoordinatesWihoutIntersection(n: number, squareSize: Size, radius = 16) : Coordinate[] {
+export function getRandomNodes(n: number, squareSize: Size, radius = 16) : Coordinate[] {
     const coordinates: Coordinate[] = [];
     const maxAttempts = 1000;
     let attempts = 0;
@@ -29,8 +30,8 @@ export function getRandomCoordinatesWihoutIntersection(n: number, squareSize: Si
     const distance = (c1: Coordinate, c2: Coordinate) => Math.sqrt(Math.pow(c1.x - c2.x, 2) + Math.pow(c1.y - c2.y, 2));
 
     while (coordinates.length < n && attempts < maxAttempts) {
-        const coordinate = getRandomCoordinate(squareSize);
-        if (coordinates.every(c => distance(c, coordinate) > radius * 2)) {
+        const coordinate = getRandomCoordinate(squareSize, radius*3);
+        if (coordinates.every(c => distance(c, coordinate) > radius * 3)) {
             coordinates.push(coordinate);
         }
         attempts++;
@@ -43,9 +44,15 @@ export function getRandomCoordinatesWihoutIntersection(n: number, squareSize: Si
     return coordinates;
 }
 
-function getRandomCoordinate(squareSize: Size) : Coordinate {
+/**
+ * Get random coordinate in a square of size `squareSize` considering 'radius' of the node.
+ * @param squareSize Size of the square, limits the max value of the coordinates.
+ * @param radius Radius of a node to be considered.
+ * @returns Random coordinate in the square.
+ */
+function getRandomCoordinate(squareSize: Size, radius: number) : Coordinate {
     return {
-        x: Math.random() * squareSize.width,
-        y: Math.random() * squareSize.height
+        x: Math.floor(Math.random() * (squareSize.width - radius+1)) + radius,
+        y: Math.floor(Math.random() * (squareSize.height - radius+1)) + radius
     }
 }
