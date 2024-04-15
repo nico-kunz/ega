def5056c4bdf9c7f8b5e25c3c4e49338610ab4f6
Nico Kunz <nicokunznk00@gmail.com>
2024-04-16 01:44:34 +0200

    Reformat App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,68 +4,71 @@ import { VNetworkGraph, VEdgeLabel } from 'v-network-graph';
 import { reactive } from 'vue';
 import { makeRandomMaxFlowGraph } from './utils/RandomMaxFlowGenerator';
   
-  const configs = reactive(
+const configs = reactive(
     defineConfigs({
-      view: {
+    view: {
         scalingObjects: true,
         autoPanAndZoomOnLoad: "center-zero"
-      },
-      node: {
+    },
+    edge: {
         label: {
-          color: "#FFF",
-          direction: "center",
-          text: (node) => {
+        background: {
+            color: '#FFFFFFFF',
+            padding: 1,
+            visible: true
+        }
+        }
+    },
+    node: {
+        label: {
+        color: "#FFF",
+        direction: "center",
+        text: (node) => {
             if(node.name === "1")
-              return "S";
+            return "S";
             if(node.name === n.toString())
-              return "D";
+            return "D";
 
             return node.name as string;
-          }
         }
-      }
+        }
+    }
     })
-  )
+)
 
-  const nodes : Nodes = reactive({
-    
-  })
+const nodes : Nodes = reactive({
 
-  const layout : Layouts = reactive({
-    nodes: {
+})
 
-    },
-  })
+const layout : Layouts = reactive({
+nodes: {
 
-  
-  const n = 10;
-  const graph = makeRandomMaxFlowGraph(n, 20, {width: 800, height: 600});
-  const layoutNodes = graph.layout.nodes;
-  for (const node in layoutNodes) {
+},
+})
+
+
+const n = 20;
+const graph = makeRandomMaxFlowGraph(n, 20, {width: 800, height: 600});
+const layoutNodes = graph.layout.nodes;
+for (const node in layoutNodes) {
     nodes[node] = { name: node };
     layout.nodes[node] = { x: layoutNodes[node].x, y: layoutNodes[node].y };
-  }
+}
 
-  const edges : Edges = reactive({
+const edges : Edges = reactive({
     ...graph.edges
-  })
+})
 
-  function addNode() {
+function addNode() {
     const newNode = `${Object.keys(nodes).length + 1}`;
     nodes[newNode] = { name: newNode };
     layout.nodes[newNode] = { x: -34.55937750784574, y: -10.346841686848222 };
     edges[`edge${Object.keys(edges).length + 1}`] = { source: "1", target: newNode };
-  }
+}
 </script>
 
 <template>
-  <v-network-graph
-    class="graph"
-    :nodes="nodes"
-    :edges="edges"
-    :layouts="layout"
-    :configs="configs"
-  >
+  <v-network-graph class="graph" :nodes="nodes" :edges="edges" :layouts="layout" :configs="configs">
     <template #edge-label="{ edge, ...slotProps }">
       <v-edge-label :text="edge.label" align="center" vertical-align="center" v-bind="slotProps" />
     </template>
