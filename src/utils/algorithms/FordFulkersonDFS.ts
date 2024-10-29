bdb53917070f0fc3998b6a6a7f9481b320da0a1f
Nico Kunz <nicokunznk00@gmail.com>
2024-10-29 01:29:21 +0100

    Update FordFulkerson to allow for step-by-step

--- src/utils/algorithms/FordFulkersonDFS.ts
+++ src/utils/algorithms/FordFulkersonDFS.ts
@@ -1,10 +1,10 @@
 import { MatrixGraph } from "../MatrixGraph";
 
-export function fordFulkerson(graph: MatrixGraph, source: number, sink: number) {
+export function fordFulkerson(graph: MatrixGraph, source: number, sink: number, update: Function, waitForNextStep: Function): Promise<number> {
     const residual = graph.capacity.map(row => row.slice())
     let maxFlow = 0
 
-    function dfs(u: number, visited: boolean[], pathFlow: number): number {
+    async function dfs(u: number, visited: boolean[], pathFlow: number): Promise<number> {
         if (u == sink) return pathFlow
 
         visited[u] = true
@@ -12,11 +12,15 @@ export function fordFulkerson(graph: MatrixGraph, source: number, sink: number) 
         for(let v = 0; v < graph.n; v++) {
             if(!visited[v] && residual[u][v] > 0) {
                 const flow = Math.min(pathFlow, residual[u][v])
-                const resultFlow = dfs(v, visited, flow)
+                const resultFlow = await dfs(v, visited, flow)
 
                 if(resultFlow > 0) {
                     residual[u][v] -= resultFlow
                     residual[v][u] += resultFlow
+
+                    update(u, v, resultFlow, residual[u][v])
+                    update(v, u, -resultFlow, residual[v][u])
+                    await waitForNextStep()
                     return resultFlow
                 }
             }
@@ -24,14 +28,21 @@ export function fordFulkerson(graph: MatrixGraph, source: number, sink: number) 
 
         return 0
     }
+    return new Promise(async(resolve) => {
+        let x = 0
+        while(true) {
+            console.log("STEP", x)
+            x++
+            const visited = Array(graph.n).fill(false)
+            const pathFlow = await dfs(source, visited, Infinity)
+    
+            if(pathFlow == 0) break
+            maxFlow += pathFlow
+            // await waitForNextStep()
+        }
+        
+        console.log(residual)
+        resolve(maxFlow)
+    })
 
-    while(true) {
-        const visited = Array(graph.n).fill(false)
-        const pathFlow = dfs(source, visited, Infinity)
-
-        if(pathFlow == 0) break
-        maxFlow += pathFlow
-    }
-
-    return maxFlow
 }
